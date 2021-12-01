77f286787cab774b0c74521ee7b3eeac3dc92b8a
alam0727 <88898127+alam0727@users.noreply.github.com>
2021-12-01 20:00:13 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,64 @@
+<footer class="footer widget-footer bg-img11 ttm-bgcolor-black ttm-bg ttm-bgimage-yes clearfix footer_div_new">
+    <div class="ttm-row-wrapper-bg-layer ttm-bg-layer"></div>
+        <div class="first-footer ttm-textcolor-white"></div>
+    <div class="second-footer">
+        <div class="container">
+            <div class="second-footer-inner">
+                <div class="row">
+                    <div class="widget-area col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                        <div class="image_box">
+                            <img id="logo-img" class="img-center" src="images/logo1.png" alt="logo-img">
+                        </div>
+                        <div class="content_Div_footer">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                    <div class="widget-area col-xs-12 col-sm-6 col-md-6 col-lg-2">
+                        <div class="widget widget_nav_menu clearfix">
+                            <h4 class="widget-title">Quick Links</h4>
+                            <ul class="menu-footer-services">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="about-us.php">About Us</a></li>
+                                <li><a href="gallery.php">Gallery</a></li>
+                                <li><a href="team.php">Team</a></li>
+                                <li><a href="blog.php">Blog</a></li>
+                                <li><a href="contact-us.php">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="widget-area col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="widget widget_nav_menu clearfix">
+                            <h4 class="widget-title">Our Services </h4>
+                            <ul class="menu-footer-services">
+                                <li><a href="#">DHOL PLAYERS</a></li>
+                                <li><a href="#">Masters of Ceremonies</a></li>
+                                <li><a href="#">Lighting Package</a></li>
+                                <li><a href="#">Disc Jockeys </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="widget-area col-xs-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="widget widget-out-link clearfix">
+                            <h4 class="widget-title">Contact Us</h4>
+                            <ul class="widget-contact">
+                                <li><i class="fa fa-map-marker"></i>Road Delhi xyz<br>24 Fifth st., Los Angeles, IN</li>
+                                <li><i class="fa fa-envelope-o"></i><a href="#">info@example.com</a></li>
+                                <li><i class="fa fa-phone"></i>Phone: +91 000 000 000 </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-footer-text">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-12 col-xs-12 col-md-4 ttm-footer2-right">
+                    <P class="text-center">Copyright © 2020-2021. All rights reserved.</P>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
